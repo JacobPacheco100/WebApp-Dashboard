849d5e5d4771df5706b3ba4897b26c25b578ab24
Jacob Pacheco <72306809+JacobPacheco100@users.noreply.github.com>
2022-06-19 13:56:06 -0400

    minor adjustments/additions

--- app.js
+++ app.js
@@ -1,140 +1,148 @@
-const trafficCanvas = document.getElementById('traffic-chart')
-const dailyCanvas = document.getElementById('daily-chart')
+const trafficCanvas = document.getElementById("traffic-chart")
+const dailyCanvas = document.getElementById("daily-chart")
 const mobileCanvas = document.getElementById("mobile-chart")
 
 //traffic
 
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
-    "4-10", "11-17", "18-24", "25-31"],
-    datasets: [{
-    data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
-    2500],
-    backgroundColor: 'rgba(116, 119, 191, .3)',
-    borderWidth: 1,
-    }]
-    };
+  labels: [
+    "16-22",
+    "23-29",
+    "30-5",
+    "6-12",
+    "13-19",
+    "20-26",
+    "27-3",
+    "4-10",
+    "11-17",
+    "18-24",
+    "25-31",
+  ],
+  datasets: [
+    {
+      data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+      backgroundColor: "rgba(116, 119, 191, .3)",
+      borderWidth: 1,
+    },
+  ],
+}
 
 let trafficOptions = {
-    tension: 0.4,
-    backgroundColor: 'rgba(112, 104, 201, .5)',
-    fill: true,
-    aspectRatio: 2.5,
-    scales: {
+  tension: 0.4,
+  backgroundColor: "rgba(112, 104, 201, .5)",
+  fill: true,
+  aspectRatio: 2.5,
+  scales: {
     y: {
-    beginAtZero: true
-    }
+      beginAtZero: true,
     },
-    plugins: {
+  },
+  plugins: {
     legend: {
-    display: false
-    }
-    }
-    };
+      display: false,
+    },
+  },
+}
 
 let trafficChart = new Chart(trafficCanvas, {
-    type: 'line',
-    data: trafficData,
-    options: trafficOptions
-    });
+  type: "line",
+  data: trafficData,
+  options: trafficOptions,
+})
 
 // daily
 
 const dailyData = {
-    labels: ["S", "M", "T", "W", "T", "F", "S"],
-    datasets: [{
-    label: '# of Hits',
-    data: [75, 115, 175, 125, 225, 200, 100],
-    backgroundColor: '#7477BF',
-    borderWidth: 1
-    }]
-    };
-    const dailyOptions = {
-    scales: {
-    y: {
-    beginAtZero: true
-    }
+  labels: ["S", "M", "T", "W", "T", "F", "S"],
+  datasets: [
+    {
+      label: "# of Hits",
+      data: [75, 115, 175, 125, 225, 200, 100],
+      backgroundColor: "#7477BF",
+      borderWidth: 1,
     },
-    plugins: {
+  ],
+}
+const dailyOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
     legend: {
-    display: false
-    }
-    }
-    };
+      display: false,
+    },
+  },
+}
 
 let dailyChart = new Chart(dailyCanvas, {
-    type: 'bar',
-    data: dailyData,
-    options: dailyOptions
-    });
+  type: "bar",
+  data: dailyData,
+  options: dailyOptions,
+})
 
 // mobile
 
 const mobileData = {
-    labels: ["Desktop", "Tablet", "Phones"],
-    datasets: [{
-    label: '# of Users',
-    data: [2000, 550, 500],
-    borderWidth: 0,
-    backgroundColor: [
-    '#7477BF',
-    '#78CF82',
-    '#51B6C8'
-    ]
-    }]
-    };
+  labels: ["Desktop", "Tablet", "Phones"],
+  datasets: [
+    {
+      label: "# of Users",
+      data: [2000, 550, 500],
+      borderWidth: 0,
+      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"],
+    },
+  ],
+}
 
 const mobileOptions = {
-    aspectRatio: 1.9,
-    plugins: {
+  aspectRatio: 1.9,
+  plugins: {
     legend: {
-    position: 'right',
-    labels: {
-    boxWidth: 20,
-    fontStyle: 'bold'
-    }
-    }
-    }
-    };
+      position: "right",
+      labels: {
+        boxWidth: 20,
+        fontStyle: "bold",
+      },
+    },
+  },
+}
 
 let mobileChart = new Chart(mobileCanvas, {
-    type: 'doughnut',
-    data: mobileData,
-    options: mobileOptions
-    });
+  type: "doughnut",
+  data: mobileData,
+  options: mobileOptions,
+})
 
 // Alert close
 
-const close = document.getElementById('close')
-const alertMessage = document.querySelector('.alert')
+const close = document.getElementById("close")
+const alertMessage = document.querySelector(".alert")
+const alertStatus = document.getElementById("alert-status")
 
-close.addEventListener('click', () => {
-    alertMessage.classList.toggle('close')
+close.addEventListener("click", () => {
+  alertMessage.classList.toggle("close")
+  alertStatus.classList.toggle("close")
 })
 
 // Form functionality
 
-const form = document.querySelector('form')
-const input = document.getElementById('input')
-const message = document.getElementById('message')
-const send = document.getElementById('send')
+const form = document.querySelector("form")
+const input = document.getElementById("input")
+const message = document.getElementById("message")
+const send = document.getElementById("send")
 
-send.addEventListener('click', ()=> {
-    if (input.value.length > 0 && message.value.length > 0){
-        alert('Your message has been sent :)')
-        input.value = ''
-        message.value = ''
-    } else {
-        alert('Input must be filled!')
-    }
+send.addEventListener("click", () => {
+  if (input.value.length > 0 && message.value.length > 0) {
+    alert("Your message has been sent :)")
+    input.value = ""
+    message.value = ""
+  } else if (input.value.length <= 0 && message.value.length > 0) {
+    alert("User must be filled!")
+  } else if (input.value.length > 0 && message.value.length <= 0) {
+    alert("Message must be filled!")
+  } else {
+    alert("all inputs must be filled out!")
+  }
 })
-
-// form.addEventListener('click', (e)=> {
-//     if (e.target === send && input.value.length > 0 && message.value.length > 0){
-//         alert('Your message has been sent :)')
-//         input.value = ''
-//         message.value = ''
-//     } else {
-//         alert('Input must be filled!')
-//     }
-// })
